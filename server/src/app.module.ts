1b9bf05d5e63960ddedbad346e9971287f86db65
KateZ <hello.katezhang@gmail.com>
2025-06-12 22:03:13 -0400

    remove not used swagger doc code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ApiModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
