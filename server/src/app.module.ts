472625e968f8086832d32ee1c04f9c8bac204c49
KateZ <hello.katezhang@gmail.com>
2025-06-12 22:05:30 -0400

    Add health module for render

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { ApiModule } from './api/api.module';
 import { DatabaseModule } from './database/database.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ApiModule, DatabaseModule],
+  imports: [ApiModule, DatabaseModule, HealthModule],
 })
 export class AppModule {}
